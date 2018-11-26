bea5184645441fd8ea7d5040cb24f8cf264411c2
sekky <pocopocoboobee@gmail.com>
2018-11-26 05:11:23 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,9 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :favorites
+  has_many :be_likeds, through: :favorites, source: :user
+  
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
