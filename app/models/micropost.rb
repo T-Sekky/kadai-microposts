f0af185ba9bdfb289078f9a69cdf693f8e201f2a
sekky <pocopocoboobee@gmail.com>
2018-11-26 05:25:11 +0000

    dependent: :destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :be_likeds, through: :favorites, source: :user
   
   
